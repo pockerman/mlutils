7f526f29210d9a405af2f915ecd710b4347ba21e
pockerman <a.giavaras@gmail.com>
2023-10-25 14:39:14 +0100

    Add requirements and utility script for tests

---
+++ local_test_pipeline.sh
@@ -0,0 +1,11 @@
+echo "Running flake8..."
+flake8 --per-file-ignores="__init__.py:F401" --max-line-length 120 mlutils/
+
+echo "Running tests with coverage..."
+coverage run -m --source=.  pytest  tests/
+
+echo "Producing coverage report..."
+coverage report -m > coverage_report.txt
+echo "Done..."
+
+
