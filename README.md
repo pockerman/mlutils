e2baf3e7f6b661174e01c692d059fca104c5abfd
pockerman <a.giavaras@gmail.com>
2023-10-26 16:54:27 +0100

    Update requirements and actions workflow

--- README.md
+++ README.md
@@ -4,6 +4,15 @@ Easy to use utilities for ml modeling.
 
 ## Installation
 
-```commandline
-pip install .
+You can install the ```mlutilities``` using ```pip```. Clone the ```main``` branch of the repository
+and build the utilities using
+
+```
+python -m build
+```
+
+Then install the built wheel using
+
+```
+pip install dist/simplemlutils-<version>.whl
 ```
--- local_test_pipeline.sh
+++ local_test_pipeline.sh
@@ -2,7 +2,7 @@ echo "Running flake8..."
 flake8 --per-file-ignores="__init__.py:F401" --max-line-length 120 mlutils/
 
 echo "Running tests with coverage..."
-coverage run -m --source=.  pytest  --verbose tests/
+coverage run -m --source=.  pytest  --verbose --junitxml="test_result.xml" tests/
 
 echo "Producing coverage report..."
 coverage report -m > coverage_report.txt
