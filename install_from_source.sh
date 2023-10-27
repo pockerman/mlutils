661ad82bc7764a95ed722396da8359bb3ac08da2
pockerman <a.giavaras@gmail.com>
2023-10-27 10:51:08 +0100

    Update installation script

--- install_from_source.sh
+++ install_from_source.sh
@@ -1,2 +1,8 @@
+# remove any previously installed versions
+pip uninstall simplemlutils
+
+# build the new package
 python3 -m build
+
+# install it
 pip install dist/simplemlutils-0.0.1-py3-none-any.whl
