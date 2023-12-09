5ee33d6f099961bc3048e8bb5ca6c5eed92777c4
pockerman <a.giavaras@gmail.com>
2023-12-09 10:52:05 +0000

    Update version strings

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="mlutils",
-      version="0.0.5",
+      version="0.0.6",
       description="Statistics and machine learning utilities",
       author="Alex Giavaras.",
       packages=['mlutils'],
